1411b5b1e6489159cb398328df99104fa1cb2528
Alfredo <63760431+AlfredoSanc@users.noreply.github.com>
2026-02-17 20:53:57 -0600

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,13 +1,14 @@
 package com.demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] args) {
+        // Inicia un servidor web en el puerto 8080
+        Javalin app = Javalin.create().start(8080);
+        
+        app.get("/", ctx -> ctx.result("¡Hola Munod!"));
+        
+        System.out.println("Aplicación iniciada en puerto 8080...");
     }
 }
